6f0f742c31e7ae1c32970b46571131a211f8c390
Sarolta <103893876+Niernen22@users.noreply.github.com>
2024-05-21 11:08:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -109,9 +109,13 @@ def edit_steps(test_id):
         cursor.execute(sql)
         modules = [row[0] for row in cursor.fetchall()]
 
+        sql = "SELECT DISTINCT(OBJECT_NAME) FROM DBA_PROCEDURES"
+        cursor.execute(sql)
+        func_proc_names = [row[0] for row in cursor.fetchall()]
+
         cursor.close()
 
-        return render_template('edit_steps.html', test_id=test_id, test_steps=test_steps_data, usernames=usernames, modules=modules)
+        return render_template('edit_steps.html', test_id=test_id, test_steps=test_steps_data, usernames=usernames, modules=modules, func_proc_names=func_proc_names)
 
     except oracledb.Error as error:
         return f"Error retrieving test steps: {error}"
@@ -180,6 +184,23 @@ def get_tables_for_schema():
         return json.dumps(table_names)
     except Exception as e:
         return json.dumps({'error': str(e)})
+
+
+@app.route('/get_parameters_details_for_funcproc', methods=['POST'])
+def get_parameters_details_for_funcproc():
+    selected_funcproc = request.json['funcproc']
+    try:
+        connection = pool.acquire()
+        cursor = connection.cursor()
+        sql = "SELECT argument_name, data_type, defaulted, default_value FROM dba_arguments WHERE IN_OUT = 'IN' AND object_name = :funcproc"
+        cursor.execute(sql, {'funcproc': selected_funcproc})
+        parameter_details = [{'argument_name': row[0], 'data_type': row[1], 'defaulted': row[2], 'default_value': row[3]} for row in cursor.fetchall()]
+        cursor.close()
+        pool.release(connection)
+        return json.dumps(parameter_details)
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
 
 
 @app.route('/get_workdays', methods=['GET'])
