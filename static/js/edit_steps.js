20678c56a2b1051add6499af62e2ddf7120b4350
Sarolta <103893876+Niernen22@users.noreply.github.com>
2024-09-19 14:22:59 +0200

    Update edit_steps.js

--- static/js/edit_steps.js
+++ static/js/edit_steps.js
@@ -2,22 +2,18 @@ function showParametersForm() {
     let selectedType = document.getElementById('step_type').value;
     if (selectedType === 'TABLECOPY') {
         document.getElementById('parametersForm').style.display = 'block';
-        document.getElementById('generateSQLButton').style.display = 'inline-block'; 
         document.getElementById('LMparametersForm').style.display = 'none';
         document.getElementById('StoredProcedureForm').style.display = 'none';
     } else if (selectedType === 'LM_JOB') {
         document.getElementById('parametersForm').style.display = 'none';
-        document.getElementById('generateSQLButton').style.display = 'inline-block'; 
         document.getElementById('LMparametersForm').style.display = 'block';
         document.getElementById('StoredProcedureForm').style.display = 'none';
     } else if (selectedType === 'STORED_PROCEDURE') {
-        document.getElementById('parametersForm').style.display = 'none';
-        document.getElementById('generateSQLButton').style.display = 'none'; 
+        document.getElementById('parametersForm').style.display = 'none'; 
         document.getElementById('LMparametersForm').style.display = 'none';
         document.getElementById('StoredProcedureForm').style.display = 'block';
     } else {
         document.getElementById('parametersForm').style.display = 'none';
-        document.getElementById('generateSQLButton').style.display = 'none'; 
         document.getElementById('LMparametersForm').style.display = 'none';
         document.getElementById('StoredProcedureForm').style.display = 'none';
     }
@@ -28,15 +24,12 @@ function showSPParametersForm() {
     let selectedType = document.getElementById('storedprocedure_type').value;
     if (selectedType === 'Function_or_Procedure') {
         document.getElementById('FunctionOrProcedureForm').style.display = 'block';
-        document.getElementById('generateSQLButtonSP').style.display = 'inline-block'; 
         document.getElementById('PackageForm').style.display = 'none';
     } else if (selectedType === 'Package') {
-        document.getElementById('FunctionOrProcedureForm').style.display = 'none';
-        document.getElementById('generateSQLButtonSP').style.display = 'inline-block'; 
+        document.getElementById('FunctionOrProcedureForm').style.display = 'none'; 
         document.getElementById('PackageForm').style.display = 'block';
     } else {
         document.getElementById('FunctionOrProcedureForm').style.display = 'none';
-        document.getElementById('generateSQLButtonSP').style.display = 'none'; 
         document.getElementById('PackageForm').style.display = 'none';
     }
 }
@@ -282,107 +275,6 @@ function getTypesForModule(selectedModule) {
 }
 
 
-function generateSQL() {
-    var selectedType = document.getElementById('step_type').value;
-    if (selectedType === 'TABLECOPY') {
-        var sourceSchema = document.getElementById('source_schema').value;
-        var sourceTable = document.getElementById('source_table').value;
-        var targetSchema = document.getElementById('target_schema').value;
-        var targetTable = document.getElementById('target_table').value;
-        var truncate = document.getElementById('truncate').value;
-        var chosenDate = document.getElementById('date').value;
-        
-        var sqlCode = "BEGIN ";
-        sqlCode += "TABLECOPY_PACKAGE.TABLECOPY(";
-        sqlCode += "'" + sourceSchema + "', ";
-        sqlCode += "'" + sourceTable + "', ";
-        sqlCode += "'" + targetSchema + "', ";
-        sqlCode += "'" + targetTable + "', ";
-        sqlCode += "'" + truncate + "', ";
-        sqlCode += "TO_DATE('" + chosenDate + "','yyyy-mm-dd')); END;";
-        
-        return sqlCode;
-
-    } else if (selectedType === 'LM_JOB') {
-        var type = document.getElementById('type').value;
-        var module = document.getElementById('module').value;
-        var name = document.getElementById('name').value;
-        
-        console.log("LM_JOB parameters:", { type, module, name });
-
-        var sqlCode = "declare ";
-        sqlCode += "p_result varchar2(4000); ";
-        sqlCode += "p_err_code varchar2(4000); ";
-        sqlCode += "p_output clob; ";
-        sqlCode += "begin ";
-        sqlCode += "lm." + type + ".execute(1, '" + module + "', '" + name + "', p_result, p_err_code, p_output, false); ";
-        sqlCode += "dbms_output.put_line(p_result || ' - ' || p_err_code); ";
-        sqlCode += "dbms_output.put_line(p_output); ";
-        sqlCode += "end; ";
-        
-        console.log("Generated SQL for LM_JOB:", sqlCode);
-        return sqlCode;
-    }
-}
-
-
-function generateAndDisplaySQL() {
-    var sqlCode = generateSQL(); 
-    console.log(sqlCode); 
-    var sqlDisplayDiv = document.getElementById('sql_code_display');
-    sqlDisplayDiv.innerText = sqlCode;
-    sqlDisplayDiv.style.display = 'block';
-}
-
-
-function generateSQLSP() {
-    var selectedType = document.getElementById('storedprocedure_type').value;
-    if (selectedType === 'Function_or_Procedure') {
-        var selectedSchema = document.getElementById('procedures_schema').value;
-        var selectedStoredObjectName = document.getElementById('storedobject_name').value;
-        var parameterValues = Array.from(document.getElementById('parameters').getElementsByTagName('input')).map(function(input) {
-        return input.value;
-        }).join(', ');
-
-        var sqlCode = "BEGIN " + selectedSchema + "." + selectedStoredObjectName + "(" + parameterValues + "); END;";
-        
-        return sqlCode;
-
-    } else if (selectedType === 'Package') {
-        var selectedSchema = document.getElementById('procedures_schema_package').value;
-        var selectedPackage = document.getElementById('storedpackage_name').value;
-        var selectedStoredObjectName = document.getElementById('storedobject_name_package').value;
-        var parameterValues = Array.from(document.getElementById('parameters_package').getElementsByTagName('input')).map(function(input) {
-        return input.value;
-        }).join(', ');
-
-        var sqlCode = "BEGIN " + selectedSchema + "." + selectedPackage + "." + selectedStoredObjectName + "(" + parameterValues + "); END;";
-        
-        return sqlCode;
-    }
-}
-
-
-
-function generateAndDisplaySQLSP() {
-    var sqlCode = generateSQLSP(); 
-    console.log(sqlCode); 
-    var sqlDisplayDiv = document.getElementById('sql_code_display');
-    sqlDisplayDiv.innerText = sqlCode;
-    sqlDisplayDiv.style.display = 'block';
-}
-
-
-function sendSQLToParent() {
-    var sqlCode = document.getElementById('sql_code_display').innerText;
-    var selectedType = document.getElementById('step_type').value;
-    document.getElementById('new_sql_code').value = sqlCode;
-    document.getElementById('new_type').value = selectedType;
-
-    document.getElementById("myModal").style.display = "none";
-}
-
-
 function openPopup() {
     var modal = document.getElementById("myModal");
 
@@ -399,11 +291,81 @@ function openPopup() {
             modal.style.display = "none";
         }
     }
-    var sqlCode = generateSQL();
-    var sqlDisplayDiv = document.getElementById('sql_code_display'); 
-    if (sqlDisplayDiv) {
-        sqlDisplayDiv.innerText = sqlCode; 
-    } else {
-        console.error('Error: Unable to find sql_code_display div in modal');
+}
+
+
+
+function submitFormData() {
+    let formData = {
+        new_step_name: document.getElementById('new_step_name').value,
+        step_type: document.getElementById('step_type').value,
+        source_schema: null,
+        source_table: null,
+        target_schema: null,
+        target_table: null,
+        truncate: null,
+        date: null,
+        module: null,
+        type: null,
+        name: null,
+        storedprocedure_type: null,
+        procedures_schema: null,
+        storedobject_name: null,
+        procedures_schema_package: null,
+        storedpackage_name: null,
+        storedobject_name_package: null,
+        parameters: {}
+    };
+    
+    if (formData.step_type === 'TABLECOPY') {
+        formData.source_schema = document.getElementById('source_schema').value.trim();
+        formData.source_table = document.getElementById('source_table').value.trim();
+        formData.target_schema = document.getElementById('target_schema').value.trim();
+        formData.target_table = document.getElementById('target_table').value.trim();
+        formData.truncate = document.getElementById('truncate').value.trim();
+        formData.date = document.getElementById('date').value.trim();
+    } else if (formData.step_type === 'LM_JOB') {
+        formData.module = document.getElementById('module').value.trim();
+        formData.type = document.getElementById('type').textContent.trim();
+        formData.name = document.getElementById('name').value.trim();
+    } else if (formData.step_type === 'STORED_PROCEDURE') {
+        formData.storedprocedure_type = document.getElementById('storedprocedure_type').value.trim();
+        if (formData.storedprocedure_type === 'Function_or_Procedure') {
+            formData.procedures_schema = document.getElementById('procedures_schema').value.trim();
+            formData.storedobject_name = document.getElementById('storedobject_name').value.trim();
+            formData.parameters = Array.from(document.getElementById('parameters').getElementsByTagName('input')).reduce((acc, input) => {
+                acc[input.name] = input.value.trim();
+                return acc;
+            }, {});
+        } else if (formData.storedprocedure_type === 'Package') {
+            formData.procedures_schema_package = document.getElementById('procedures_schema_package').value.trim();
+            formData.storedpackage_name = document.getElementById('storedpackage_name').value.trim();
+            formData.storedobject_name_package = document.getElementById('storedobject_name_package').value.trim();
+            formData.parameters = Array.from(document.getElementById('parameters_package').getElementsByTagName('input')).reduce((acc, input) => {
+                acc[input.name] = input.value.trim();
+                return acc;
+            }, {});
+        }
     }
+
+    fetch('/add_step/' + test_id, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert("Step successfully added!");
+            window.location.href = data.redirect_url;
+        } else {
+            alert("Error: " + data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Error during AJAX submission:', error);
+        alert("There was an error submitting the form. Please try again.");
+    });
 }
