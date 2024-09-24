2ebb71d142b6e640def13df8b4811fc2c48d7702
Sarolta <103893876+Niernen22@users.noreply.github.com>
2024-09-24 14:17:21 +0200

    Update test_steps.js

--- static/js/test_steps.js
+++ static/js/test_steps.js
@@ -1,5 +1,14 @@
-document.getElementById('RunPlanButton').addEventListener('click', function() {
-    setTimeout(function() {
-        location.reload();
-    }, 5000);
+document.getElementById('RunPlanButton').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const test_id = window.location.pathname.split('/').pop();
+    
+    fetch(`/run_test/${test_id}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).finally(() => {
+        window.location.href = `/test_steps/${test_id}`;
+    });
 });
