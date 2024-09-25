9726f2a031f5c58150fa55e23358a9922c8c2a18
Sarolta <103893876+Niernen22@users.noreply.github.com>
2024-09-25 10:59:56 +0200

    Update test_steps.js

--- static/js/test_steps.js
+++ static/js/test_steps.js
@@ -2,13 +2,23 @@ document.getElementById('RunPlanButton').addEventListener('click', function(even
     event.preventDefault();
 
     const test_id = window.location.pathname.split('/').pop();
-    
+
     fetch(`/run_test/${test_id}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         }
-    }).finally(() => {
-        window.location.href = `/test_steps/${test_id}`;
-    });
+    }).then(response => response.json())
+      .then(data => {
+          if (data.success) {
+              alert(`Test started successfully! Refresh the page or check the logs for updates. Run ID: ${data.v_run_id}`);
+
+              window.location.href = `/test_steps/${test_id}`;
+          } else {
+              alert('Error starting test: ' + data.error);
+          }
+      })
+      .catch(error => {
+          alert('An unexpected error occurred: ' + error);
+      });
 });
